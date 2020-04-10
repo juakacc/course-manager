d83a48d706bf90a48b376826e471ec40f610f089
Juaka <juakacc@gmail.com>
2020-04-10 20:45:19 -0300

    Trabalhando com rotas e formulários, tratamento de rota inválida e Pipes

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -8,6 +8,17 @@ export class CourseService {
   retrieveAll(): Course[] {
     return COURSES;
   }
+
+  retrieveById(id: number): Course {
+    return COURSES.find((course: Course) => course.id == id);
+  }
+
+  save(course: Course): void {
+    if (course.id) {
+      const index = COURSES.findIndex((c: Course) => c.id == course.id);
+      COURSES[index] = course;
+    }
+  }
 }
 
 var COURSES: Course[] = [
